5aecdb69f9e83c8dd15b38a45d84661919a3680c
Sebastian <sebastiansloma@gmail.com>
2023-03-27 08:42:35 +0200

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Header from './header/Header'
+import Nav from './nav/Nav'
+import About from './about/About'
+import Expirience from './expirience/Expirience'
+import Services from './services/Services'
+import Portfolio from './portfolio/Portfolio'
+import Testimonials from './testimonials/Testimonials'
+import Contact from './contact/Contact'
+import Footer from './footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Expirience/>
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </>
   )
 }
 
---
+++ src/expirience/Expirience.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './expirience.css'
+
+const Expirience = () => {
+  return (
+    <div>Expirience</div>
+  )
+}
+
+export default Expirience
