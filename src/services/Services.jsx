5bbe1fd22c597a9d6377cd40d577b7fc0e4c6add
Sebastian <sebastiansloma@gmail.com>
2023-03-28 15:40:24 +0200

    added services component jsx

--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
-import './services.css'
-
+import React from 'react';
+import './services.css';
+import { BiCheck } from 'react-icons/bi';
 
 const Services = () => {
-  return (
-    <section id='services'>Services</section>
-  )
-}
+	return (
+		<section id='services'>
+			<h5>What I Offer</h5>
+			<h2>Services</h2>
 
-export default Services
+			<div className='container services__container'>
+				{/* UI/UX */}
+				<article className='service'>
+					<div className='service__head'>
+						<h3>UI/UX Design</h3>
+					</div>
+
+					<ul className='service__ist'>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+					</ul>
+				</article>
+				{/* Web Dev */}
+				<article className='service'>
+					<div className='service__head'>
+						<h3>Web Development</h3>
+					</div>
+
+					<ul className='service__ist'>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+					</ul>
+				</article>
+				{/* content creation */}
+
+				<article className='service'>
+					<div className='service__head'>
+						<h3>Content Creation</h3>
+					</div>
+
+					<ul className='service__ist'>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</li>
+					</ul>
+				</article>
+			</div>
+		</section>
+	);
+};
+
+export default Services;
