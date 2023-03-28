1a4d1ecdfa58e6149b16dc2c24b4dadb5f5b7889
Sebastian <sebastiansloma@gmail.com>
2023-03-28 14:16:18 +0200

    add style css of expirience section and rwd

--- src/expirience/Expirience.jsx
+++ src/expirience/Expirience.jsx
@@ -1,53 +1,60 @@
-import React from 'react'
-import './expirience.css'
-import {BsPatchCheckFill} from 'react-icons/bs'
+import React from 'react';
+import './expirience.css';
+import { BsPatchCheckFill } from 'react-icons/bs';
 
 const Expirience = () => {
-  return (
-    <section id='expirience'>
-      <h5>What Skills I Have</h5>
-      <h2>My Expirience</h2>
+	return (
+		<section id='expirience'>
+			<h5>What Skills I Have</h5>
+			<h2>My Expirience</h2>
 
-      <div className="container expirience__container">
-        <div className="expirience__container">
-          <div className="expirience__frontend">
-            <h3>Frontend Development</h3>
-            <div className="expirience__content">
+			<div className='container expirience__container'>
+				
 
-              <article className="expirience__details">
-                <BsPatchCheckFill/>
-                <h4>JavaScrtipt</h4>
-                <small className="text-light">Expirienced</small>
-              </article>
-              <article className="expirience__details">
-                <BsPatchCheckFill/>
-                <h4>React</h4>
-                <small className="text-light">Intermediate</small>
-              </article>
-            </div>
-          </div>
+					<div className='expirience__frontend'>
+						<h3>Frontend Development</h3>
+						<div className='expirience__content'>
+							<article className='expirience__details'>
+								<BsPatchCheckFill className='expirience__details-icon'/>
+								<div>
+									<h4>JavaScrtipt</h4>
+									<small className='text-light'>Expirienced</small>
+								</div>
+							</article>
+							<article className='expirience__details'>
+								<BsPatchCheckFill className='expirience__details-icon'/>
+								<div>
+									<h4>React</h4>
+									<small className='text-light'>Intermediate</small>
+								</div>
+							</article>
+						</div>
+					</div>
 
-          <div className="expirience__backend">
-            <h3>Backend Development</h3>
-            <div className="expirience__content">
-              <article className="expirience__details">
-              <BsPatchCheckFill/>
-                <h4>Python</h4>
-                <small className="text-light">Intermediate</small>
-              </article>
-              
-              <article className="expirience__details">
-                <BsPatchCheckFill/>
-                <h4>Node JS</h4>
-                <small className="text-light">Intermediate</small>
-              </article>
-            </div>
+					<div className='expirience__backend'>
+						<h3>Backend Development</h3>
+						<div className='expirience__content'>
+							<article className='expirience__details'>
+								<BsPatchCheckFill  className='expirience__details-icon'/>
+								<div>
+									<h4>Python</h4>
+									<small className='text-light'>Intermediate</small>
+								</div>
+							</article>
 
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-}
+							<article className='expirience__details'>
+								<BsPatchCheckFill className='expirience__details-icon'/>
+								<div>
+									<h4>Node JS</h4>
+									<small className='text-light'>Intermediate</small>
+								</div>
+							</article>
+						</div>
+					</div>
+				</div>
+			
+		</section>
+	);
+};
 
-export default Expirience
+export default Expirience;
