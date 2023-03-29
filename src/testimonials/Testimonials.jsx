eedf6a6b27cf42540dbf60ebd96c71084891dbf2
Sebastian <sebastiansloma@gmail.com>
2023-03-29 14:51:52 +0200

    added swiper js

--- src/testimonials/Testimonials.jsx
+++ src/testimonials/Testimonials.jsx
@@ -2,79 +2,65 @@ import React from 'react';
 import './testimonials.css';
 import AVTR from '../assets/lee.jpg';
 
+// import Swiper core and required modules
+import { Navigation, Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+const data = [
+	{
+		avatar: AVTR,
+		name: 'Johan Snow',
+		review:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus est ab exercitationem doloremque itaque sed nostrum sit quisquam, maxime assumenda alias aliquid aliquam, cupiditate quia! Sed quo possimus dicta excepturi.',
+	},
+	{
+		avatar: AVTR,
+		name: 'Jon Snow',
+		review:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos minima rerum nostrum quibusdam architecto ratione necessitatibus porro atque voluptatem dolore! Voluptas incidunt ipsa quo. Sit excepturi harum consectetur in. Culpa!',
+	},
+	{
+		avatar: AVTR,
+		name: 'Johnatan Snow',
+		review:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, iste!',
+	},
+];
+
 const Testimonials = () => {
 	return (
 		<section id='testimonials'>
 			<h5>Review from clients</h5>
 			<h2>Testimonials</h2>
 
-			<div className='container testimonials__container'>
-				<article className='testimonial'>
-					<div className='client__avatar'>
-						<img src={AVTR} alt='Avatar' />
-					</div>
-					<h5 className='client__name'>Lee Cambel</h5>
-					<small className='client__review'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
-						repellat!
-					</small>
-				</article>
-
-				<article className='testimonial'>
-					<div className='client__avatar'>
-						<img src={AVTR} alt='Avatar' />
-					</div>
-					<h5 className='client__name'>Lee Cambel</h5>
-					<small className='client__review'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
-						repellat!
-					</small>
-				</article>
-
-				<article className='testimonial'>
-					<div className='client__avatar'>
-						<img src={AVTR} alt='Avatar' />
-					</div>
-					<h5 className='client__name'>Lee Cambel</h5>
-					<small className='client__review'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
-						repellat!
-					</small>
-				</article>
-			</div>
+			<Swiper className='container testimonials__container'
+			modules={[Navigation,Pagination]}
+			navigation
+			spaceBetween={40}
+			slidesPerView={1}
+			pagination={{ clickable: true }}
+		
+			>
+				{data.map(({ avatar, name, review }, index) => {
+					return (
+						<SwiperSlide key={index} className='testimonial'>
+							<div className='client__avatar'>
+								<img src={avatar} alt={name} />
+							</div>
+							<h5 className='client__name'>{name}</h5>
+							<small className='client__review'>{review}</small>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
 		</section>
 	);
 };
 
 export default Testimonials;
-
-// const data =[
-//   avatar: AVTR1,
-//   named: 'Jon Snow',
-//   review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos minima rerum nostrum quibusdam architecto ratione necessitatibus porro atque voluptatem dolore! Voluptas incidunt ipsa quo. Sit excepturi harum consectetur in. Culpa!'
-// ]
-
-// const Testimonials = () => {
-// 	return (
-// 		<section id='testimonials'>
-// 			<h5>Review from clients</h5>
-// 			<h2>Testimonials</h2>
-
-// 			<div className='container testimonials__container'>
-// 				{data.map(({ avatar, named, review }, index) => {
-// 					return (
-// 						<article key={index} className='testimonial'>
-// 							<div className='client__avatar'>
-// 								<img src={avatar} alt={named} />
-// 							</div>
-// 							<h5 className='client__name'>{named}</h5>
-// 							<small className='client__review'>
-// 								{review}
-// 							</small>
-// 						</article>
-// 					);
-// 				})}
-// 			</div>
-// 		</section>
-// 	);
-// };
