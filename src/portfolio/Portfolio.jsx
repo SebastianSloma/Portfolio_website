5dc8b2d72d4274f88fe07aaad4ef936000daf66d
Sebastian <sebastiansloma@gmail.com>
2023-03-29 14:00:39 +0200

    added testimonials component jsx

--- src/portfolio/Portfolio.jsx
+++ src/portfolio/Portfolio.jsx
@@ -2,42 +2,6 @@ import React from 'react';
 import './portfolio.css';
 import IMG from '../assets/unsplash.jpg';
 
-// const data =[
-//   id: 1,
-//   image: IMG1,
-//   title: 'Crypto Currency Dashboard',
-//   github: 'https://github.com',
-//   demo: 'https://drible.com'
-// ]
-// const Portfolio =()=>{
-// return(
-//     <section id='portfolio'>
-//       <h5>My Recent Work</h5>
-//       <h2>Portfolio</h2>
-
-//       <div className="container portfolio__container">
-//         {
-//           data.map(({id,image,title,github,demo})=>{
-//             return(
-//               <article key={id}className="portfolio__item">
-//           <div className="portfolio__item-image">
-//             <img src={image} alt={title} />
-//           </div>
-//           <h3>This is a portfolio item title</h3>
-//           <div className="portfolio__item-cta">
-//           <a href={github} className='btn' target='_blank' rel="noreferrer">Github</a>
-//           <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
-//           </div>
-//         </article>
-//             )
-//           })
-//         }
-
-//       </div>
-
-//     </section>
-//   )
-// }
 
 const Portfolio = () => {
 	return (
@@ -196,3 +160,41 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
+
+// const data =[
+//   id: 1,
+//   image: IMG1,
+//   title: 'Crypto Currency Dashboard',
+//   github: 'https://github.com',
+//   demo: 'https://drible.com'
+// ]
+// const Portfolio =()=>{
+// return(
+//     <section id='portfolio'>
+//       <h5>My Recent Work</h5>
+//       <h2>Portfolio</h2>
+
+//       <div className="container portfolio__container">
+//         {
+//           data.map(({id,image,title,github,demo})=>{
+//             return(
+//               <article key={id}className="portfolio__item">
+//           <div className="portfolio__item-image">
+//             <img src={image} alt={title} />
+//           </div>
+//           <h3>This is a portfolio item title</h3>
+//           <div className="portfolio__item-cta">
+//           <a href={github} className='btn' target='_blank' rel="noreferrer">Github</a>
+//           <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+//           </div>
+//         </article>
+//             )
+//           })
+//         }
+
+//       </div>
+
+//     </section>
+//   )
+// }
+
--- src/testimonials/Testimonials.jsx
+++ src/testimonials/Testimonials.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR from '../assets/lee.jpg'
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonials">
+          <div className="client__avatar">
+            <img src={AVTR} alt="Avatar" />
+            <h5 className='client__name'>Lee Cambel</h5>
+            <small className='client__review'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, repellat!
+            </small>
+          </div>
+        </article>
+        <article className="testimonials">
+          <div className="client__avatar">
+            <img src={AVTR} alt="Avatar" />
+            <h5 className='client__name'>Lee Cambel</h5>
+            <small className='client__review'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, repellat!
+            </small>
+          </div>
+        </article>
+        <article className="testimonials">
+          <div className="client__avatar">
+            <img src={AVTR} alt="Avatar" />
+            <h5 className='client__name'>Lee Cambel</h5>
+            <small className='client__review'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, repellat!
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
